e81683c549cbef749f6c6a7081f8debe43af3a03
mchudz97 <mchudz97@gmail.com>
2023-09-24 10:44:03 +0200

    AppContainer

---
+++ app/src/main/java/com/example/flightsearch/data/AppContainer.kt
@@ -0,0 +1,23 @@
+package com.example.flightsearch.data
+
+import android.content.Context
+
+interface AppContainer {
+    val airportRepository: AirportRepository
+    val favoriteRepository: FavoriteRepository
+}
+
+class AppDataContainer(
+    private val context: Context
+) : AppContainer {
+    override val airportRepository: AirportRepository by lazy {
+        OfflineAirportRepository(
+            FlightSearchDatabase.getDatabase(context).airportDao()
+        )
+    }
+    override val favoriteRepository: FavoriteRepository by lazy {
+        OfflineFavoriteRepository(
+            FlightSearchDatabase.getDatabase(context).favoriteDao()
+        )
+    }
+}
